10fbea09459b4457b8080a0d01a0ff2a8ce3478f
darkopancev <darkopancev.dev@gmail.com>
2020-08-10 17:50:30 +0800

    add test uart isrpipe test class

--- tests/unit/source/utils/isrpipe/test_isrpipe.cpp
+++ tests/unit/source/utils/isrpipe/test_isrpipe.cpp
@@ -1,5 +1,7 @@
 #include "gtest/gtest.h"
 
+#include "core/instance.hpp"
+
 #include "utils/isrpipe.hpp"
 
 using namespace vc;
@@ -10,91 +12,120 @@ class TestUtilsTsrb : public testing::Test
 protected:
     char buffer[8];
 
-    Tsrb *obj;
+    Tsrb *tsrb;
 
     virtual void SetUp()
     {
-        obj = new Tsrb(buffer, sizeof(buffer));
+        tsrb = new Tsrb(buffer, sizeof(buffer));
     }
 
     virtual void TearDown()
     {
-        delete obj;
+        delete tsrb;
+    }
+};
+
+class TestUtilsUartIsrpipe : public testing::Test
+{
+protected:
+    Instance *instance;
+    UartIsrpipe *uart_isrpipe;
+
+    virtual void SetUp()
+    {
+        instance = new Instance();
+        uart_isrpipe = new UartIsrpipe(*instance);
+    }
+
+    virtual void TearDown()
+    {
+        delete instance;
+        delete uart_isrpipe;
     }
 };
 
 TEST_F(TestUtilsTsrb, constructor_test)
 {
-    EXPECT_TRUE(obj);
+    EXPECT_TRUE(tsrb);
+}
+
+TEST_F(TestUtilsUartIsrpipe, constructor_test)
+{
+    EXPECT_TRUE(instance);
+    EXPECT_TRUE(uart_isrpipe);
 }
 
 TEST_F(TestUtilsTsrb, functions_test)
 {
-    EXPECT_EQ(obj->avail(), 0);
-    EXPECT_TRUE(obj->is_empty());
-    EXPECT_FALSE(obj->is_full());
-    EXPECT_EQ(obj->free(), 8);
+    EXPECT_EQ(tsrb->avail(), 0);
+    EXPECT_TRUE(tsrb->is_empty());
+    EXPECT_FALSE(tsrb->is_full());
+    EXPECT_EQ(tsrb->free(), 8);
 
-    obj->add_one(0x1);
-    obj->add_one(0x2);
-    obj->add_one(0x3);
-    obj->add_one(0x4);
+    tsrb->add_one(0x1);
+    tsrb->add_one(0x2);
+    tsrb->add_one(0x3);
+    tsrb->add_one(0x4);
 
-    EXPECT_EQ(obj->avail(), 4);
-    EXPECT_FALSE(obj->is_empty());
-    EXPECT_FALSE(obj->is_full());
-    EXPECT_EQ(obj->free(), 4);
+    EXPECT_EQ(tsrb->avail(), 4);
+    EXPECT_FALSE(tsrb->is_empty());
+    EXPECT_FALSE(tsrb->is_full());
+    EXPECT_EQ(tsrb->free(), 4);
 
-    obj->push(0x5);
-    obj->push(0x6);
-    obj->push(0x7);
-    obj->push(0x8);
+    tsrb->push(0x5);
+    tsrb->push(0x6);
+    tsrb->push(0x7);
+    tsrb->push(0x8);
 
-    EXPECT_EQ(obj->avail(), 8);
-    EXPECT_FALSE(obj->is_empty());
-    EXPECT_TRUE(obj->is_full());
-    EXPECT_EQ(obj->free(), 0);
+    EXPECT_EQ(tsrb->avail(), 8);
+    EXPECT_FALSE(tsrb->is_empty());
+    EXPECT_TRUE(tsrb->is_full());
+    EXPECT_EQ(tsrb->free(), 0);
 
-    EXPECT_EQ(obj->pop(), 0x1);
-    EXPECT_EQ(obj->pop(), 0x2);
-    EXPECT_EQ(obj->pop(), 0x3);
-    EXPECT_EQ(obj->pop(), 0x4);
+    EXPECT_EQ(tsrb->pop(), 0x1);
+    EXPECT_EQ(tsrb->pop(), 0x2);
+    EXPECT_EQ(tsrb->pop(), 0x3);
+    EXPECT_EQ(tsrb->pop(), 0x4);
 
-    EXPECT_EQ(obj->free(), 4);
+    EXPECT_EQ(tsrb->free(), 4);
 
-    obj->drop(2);
+    tsrb->drop(2);
 
-    EXPECT_EQ(obj->free(), 6);
-    EXPECT_EQ(obj->avail(), 2);
+    EXPECT_EQ(tsrb->free(), 6);
+    EXPECT_EQ(tsrb->avail(), 2);
 
-    EXPECT_EQ(obj->get_one(), 0x7);
-    EXPECT_EQ(obj->get_one(), 0x8);
+    EXPECT_EQ(tsrb->get_one(), 0x7);
+    EXPECT_EQ(tsrb->get_one(), 0x8);
 
-    EXPECT_EQ(obj->avail(), 0);
-    EXPECT_TRUE(obj->is_empty());
-    EXPECT_FALSE(obj->is_full());
-    EXPECT_EQ(obj->free(), 8);
+    EXPECT_EQ(tsrb->avail(), 0);
+    EXPECT_TRUE(tsrb->is_empty());
+    EXPECT_FALSE(tsrb->is_full());
+    EXPECT_EQ(tsrb->free(), 8);
 
     char data[4] = {(char)0xfa, (char)0xfb, (char)0xfc, (char)0xfd};
 
-    obj->add(data, sizeof(data));
+    tsrb->add(data, sizeof(data));
 
-    EXPECT_EQ(obj->avail(), 4);
-    EXPECT_FALSE(obj->is_empty());
-    EXPECT_FALSE(obj->is_full());
-    EXPECT_EQ(obj->free(), 4);
+    EXPECT_EQ(tsrb->avail(), 4);
+    EXPECT_FALSE(tsrb->is_empty());
+    EXPECT_FALSE(tsrb->is_full());
+    EXPECT_EQ(tsrb->free(), 4);
 
     char result[4];
 
-    obj->get(result, sizeof(result));
+    tsrb->get(result, sizeof(result));
 
-    EXPECT_EQ(obj->avail(), 0);
-    EXPECT_TRUE(obj->is_empty());
-    EXPECT_FALSE(obj->is_full());
-    EXPECT_EQ(obj->free(), 8);
+    EXPECT_EQ(tsrb->avail(), 0);
+    EXPECT_TRUE(tsrb->is_empty());
+    EXPECT_FALSE(tsrb->is_full());
+    EXPECT_EQ(tsrb->free(), 8);
 
     EXPECT_EQ(result[0], (char)0xfa);
     EXPECT_EQ(result[1], (char)0xfb);
     EXPECT_EQ(result[2], (char)0xfc);
     EXPECT_EQ(result[3], (char)0xfd);
+}
+
+TEST_F(TestUtilsUartIsrpipe, functions_test)
+{
 }
--- tests/unit/source/utils/ringbuffer/test_ringbuffer.cpp
+++ tests/unit/source/utils/ringbuffer/test_ringbuffer.cpp
@@ -10,106 +10,106 @@ class TestUtilsRingbuffer : public testing::Test
 protected:
     char buffer[8];
 
-    RingBuffer *obj;
+    RingBuffer *rb;
 
     virtual void SetUp()
     {
-        obj = new RingBuffer(buffer, sizeof(buffer));
+        rb = new RingBuffer(buffer, sizeof(buffer));
     }
 
     virtual void TearDown()
     {
-        delete obj;
+        delete rb;
     }
 };
 
 TEST_F(TestUtilsRingbuffer, constructor_test)
 {
-    EXPECT_TRUE(obj);
+    EXPECT_TRUE(rb);
 }
 
 TEST_F(TestUtilsRingbuffer, functions_test)
 {
-    EXPECT_EQ(obj->avail(), 0);
+    EXPECT_EQ(rb->avail(), 0);
 
-    obj->add_one(0x1);
-    obj->add_one(0x2);
+    rb->add_one(0x1);
+    rb->add_one(0x2);
 
-    EXPECT_EQ(obj->avail(), 2);
+    EXPECT_EQ(rb->avail(), 2);
 
-    EXPECT_NE(obj->get_one(), -1); /* this get 0x1 */
-    EXPECT_EQ(obj->get_one(), 0x2);
+    EXPECT_NE(rb->get_one(), -1); /* this get 0x1 */
+    EXPECT_EQ(rb->get_one(), 0x2);
 
-    EXPECT_TRUE(obj->is_empty());
-    EXPECT_FALSE(obj->is_full());
+    EXPECT_TRUE(rb->is_empty());
+    EXPECT_FALSE(rb->is_full());
 
-    EXPECT_EQ(obj->peek_one(), -1); /* nothing in the buffer */
+    EXPECT_EQ(rb->peek_one(), -1); /* nothing in the buffer */
 
-    obj->add_one(0x1);
-    obj->add_one(0x2);
-    obj->add_one(0x3);
-    obj->add_one(0x4);
-    obj->add_one(0x5);
-    obj->add_one(0x6);
-    obj->add_one(0x7);
-    obj->add_one(0x8);
+    rb->add_one(0x1);
+    rb->add_one(0x2);
+    rb->add_one(0x3);
+    rb->add_one(0x4);
+    rb->add_one(0x5);
+    rb->add_one(0x6);
+    rb->add_one(0x7);
+    rb->add_one(0x8);
 
-    EXPECT_EQ(obj->avail(), 8);
+    EXPECT_EQ(rb->avail(), 8);
 
-    EXPECT_FALSE(obj->is_empty());
-    EXPECT_TRUE(obj->is_full());
+    EXPECT_FALSE(rb->is_empty());
+    EXPECT_TRUE(rb->is_full());
 
-    EXPECT_EQ(obj->add_one(0x9), 0x1); /* this will remove the head (0x1) and add (0x9) to the tail */
+    EXPECT_EQ(rb->add_one(0x9), 0x1); /* this will remove the head (0x1) and add (0x9) to the tail */
 
-    EXPECT_TRUE(obj->is_full());
+    EXPECT_TRUE(rb->is_full());
 
-    EXPECT_EQ(obj->peek_one(), 0x2); /* 0x2 is the head */
-    EXPECT_EQ(obj->peek_one(), 0x2); /* 0x2 is the head */
-    EXPECT_EQ(obj->peek_one(), 0x2); /* 0x2 is the head */
-    EXPECT_EQ(obj->peek_one(), 0x2); /* 0x2 is the head */
+    EXPECT_EQ(rb->peek_one(), 0x2); /* 0x2 is the head */
+    EXPECT_EQ(rb->peek_one(), 0x2); /* 0x2 is the head */
+    EXPECT_EQ(rb->peek_one(), 0x2); /* 0x2 is the head */
+    EXPECT_EQ(rb->peek_one(), 0x2); /* 0x2 is the head */
 
-    EXPECT_EQ(obj->avail(), 8); /* peek will not reduce the buffer size */
+    EXPECT_EQ(rb->avail(), 8); /* peek will not reduce the buffer size */
 
     char peeked[4];
 
-    obj->peek(peeked, sizeof(peeked));
+    rb->peek(peeked, sizeof(peeked));
 
-    EXPECT_EQ(obj->avail(), 8);
+    EXPECT_EQ(rb->avail(), 8);
 
     EXPECT_EQ(peeked[0], 0x2);
     EXPECT_EQ(peeked[1], 0x3);
     EXPECT_EQ(peeked[2], 0x4);
     EXPECT_EQ(peeked[3], 0x5);
 
-    obj->remove(2); /* 0x2, 0x3 was removed */
+    rb->remove(2); /* 0x2, 0x3 was removed */
 
-    EXPECT_EQ(obj->avail(), 6);
+    EXPECT_EQ(rb->avail(), 6);
 
     char gotten[4];
 
-    obj->get(gotten, sizeof(gotten));
+    rb->get(gotten, sizeof(gotten));
 
-    EXPECT_EQ(obj->avail(), 2);
+    EXPECT_EQ(rb->avail(), 2);
 
     EXPECT_EQ(gotten[0], 0x4);
     EXPECT_EQ(gotten[1], 0x5);
     EXPECT_EQ(gotten[2], 0x6);
     EXPECT_EQ(gotten[3], 0x7);
 
-    EXPECT_EQ(obj->free(), 6); /* we have 6 byte of free space */
+    EXPECT_EQ(rb->free(), 6); /* we have 6 byte of free space */
 
     char added[2] = {static_cast<char>(0xff), static_cast<char>(0xfd)};
 
-    obj->add(added, sizeof(added));
+    rb->add(added, sizeof(added));
 
-    EXPECT_EQ(obj->avail(), 4);
-    EXPECT_EQ(obj->free(), 4);
+    EXPECT_EQ(rb->avail(), 4);
+    EXPECT_EQ(rb->free(), 4);
 
-    EXPECT_EQ(obj->get_one(), 0x8);
-    EXPECT_EQ(obj->get_one(), 0x9);
-    EXPECT_EQ(obj->get_one(), 0xff);
-    EXPECT_EQ(obj->get_one(), 0xfd);
+    EXPECT_EQ(rb->get_one(), 0x8);
+    EXPECT_EQ(rb->get_one(), 0x9);
+    EXPECT_EQ(rb->get_one(), 0xff);
+    EXPECT_EQ(rb->get_one(), 0xfd);
 
-    EXPECT_EQ(obj->free(), 8);
-    EXPECT_EQ(obj->avail(), 0);
+    EXPECT_EQ(rb->free(), 8);
+    EXPECT_EQ(rb->avail(), 0);
 }
